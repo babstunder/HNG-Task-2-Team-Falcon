30ce9ebb7ba0632ac37c879827d621e7f406a855
babstunder <63543336+babstunder@users.noreply.github.com>
2020-06-03 23:52:28 +0100

    Delete HNG-01600.js

--- HNG-01600.js
+++
@@ -1,4 +0,0 @@
-
-function remark(){
-     console.log("Hello World, this is Lawal Toheeb Babatunde with HNGi7 ID HNG-01600 using Javascript for stage 2 task. lawaltoheeb231@gmail.com")};
-remark();
