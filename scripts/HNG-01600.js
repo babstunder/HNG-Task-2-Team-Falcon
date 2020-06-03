5d62379a1fe8f4c37d4d0a6296c8f9cc66807add
babstunder <63543336+babstunder@users.noreply.github.com>
2020-06-03 23:53:44 +0100

    Update HNG-01600.js

--- scripts/HNG-01600.js
+++ scripts/HNG-01600.js
@@ -1,4 +1,4 @@
 
-function remark(){
+function rem(){
   console.log("Hello World, this is Lawal Toheeb Babatunde with HNGi7 ID HNG-01600 using Javascript for stage 2 task. lawaltoheeb231@gmail.com)};
-remark();
+rem();
